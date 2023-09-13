00b27977f5934d5e9bb7adf2e300a57e86c98708
Towhid Khandaker <towhidk2@gmail.com>
2023-09-13 10:51:02 +0600

    update background color

--- popup.js
+++ popup.js
@@ -1,24 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var enableButton = document.getElementById('enable-extension');
+document.addEventListener('DOMContentLoaded', function () {
+    var toggleButton = document.getElementById('disable-btn');
 
-  // Load the button state from Chrome Storage and set the initial button text accordingly
-  chrome.storage.sync.get(['isDisabled'], function(result) {
-      var isDisabled = result.isDisabled === true;
-      enableButton.innerText = isDisabled ? 'Enable' : 'Disable';
-  });
+    // Load the button state from Chrome Storage and set the initial button text accordingly
+    chrome.storage.sync.get(['isDisabled'], function (result) {
+        var isDisabled = result.isDisabled === true;
+        toggleButton.innerText = isDisabled ? 'Enable' : 'Disable';
 
-  enableButton.addEventListener('click', function() {
-      var thisButton = this;
-      var disable = thisButton.innerText === 'Disable' ? true : false;
-      thisButton.innerText = disable ? 'Enable' : 'Disable';
+        // Set the button's ID based on the state
+        toggleButton.id = isDisabled ? 'disable-btn' : 'enable-btn';
+    });
 
-      // Save the button state to Chrome Storage
-      chrome.storage.sync.set({ isDisabled: disable });
+    toggleButton.addEventListener('click', function () {
+        var thisButton = this;
+        var disable = thisButton.innerText === 'Disable' ? true : false;
+        thisButton.innerText = disable ? 'Enable' : 'Disable';
 
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, { disabled: disable });
-      });
-  });
+        // Set the button's ID based on the state
+        toggleButton.id = disable ? 'disable-btn' : 'enable-btn';
+
+        // Save the button state to Chrome Storage
+        chrome.storage.sync.set({ isDisabled: disable });
+
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { disabled: disable });
+        });
+    });
 }, false);
 
 
